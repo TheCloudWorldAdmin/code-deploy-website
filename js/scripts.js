4c86397034e0189b1b562827534fe2b266db8fc3
wilshan <74950445+wilshan@users.noreply.github.com>
2021-06-12 12:27:32 +0000

    v0.1 Upload

---
+++ js/scripts.js
@@ -0,0 +1,70 @@
+﻿/*
+	Active by TEMPLATE STOCK
+	templatestock.co @templatestock
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+
+jQuery(function($){
+
+'use strict';
+
+
+    /* ---------------------------------------------- /*
+     * Preloader
+    /* ---------------------------------------------- */
+	(function () {
+	    $(window).load(function() {
+	        $('#pre-status').fadeOut();
+	        $('#st-preloader').delay(350).fadeOut('slow');
+	    });
+	}());
+
+
+
+    /* ---------------------------------------------- /*
+     * Animated scrolling / Scroll Up
+    /* ---------------------------------------------- */
+    (function () {
+        $('a[href*=#]').bind("click", function(e){
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top
+            }, 1000);
+            e.preventDefault();
+        });
+    }());
+
+
+    /* ---------------------------------------------- /*
+     * Full Screen
+    /* ---------------------------------------------- */
+    (function () {
+        $(".st-fullHeight").height($(window).height());
+
+        $(window).resize(function(){
+            $(".st-fullHeight").height($(window).height());
+        });
+
+    }());
+	
+
+    /* ---------------------------------------------- /*
+     * Parallax Scrolling
+    /* ---------------------------------------------- */
+	(function () {
+		$(window).bind('load', function () {
+			parallaxInit();						  
+		});
+		function parallaxInit() {		
+			$(".subscribe").parallax("50%", 0.3);
+		}	
+		parallaxInit();
+	}());
+
+
+		
+
+	
+
+});
